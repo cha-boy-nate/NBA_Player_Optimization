6e3c45a1259a8dce07be952656fbc265fae91edf
Nathan <nathanj@spu.edu>
2019-03-11 14:39:42 -0700

    Update nba.py

--- nba.py
+++ nba.py
@@ -13,60 +13,60 @@ class player:
 	value = 0
 	age = 0
 
-	def __init__(self, playerID, name, value, age):
+	def __init__(self, playerID, name, value, weight):
 		self.playerID = playerID
 		self.name = name
 		self.value = value
-		self.age = age
+		self.weight = weight
 
 def main():
-	#create arrays for players sorted into positions
-	shootingGuard = []
-	powerForward = []
-	center = []
-	pointGuard = []
-	
-	#get data from csv and sort players into arrays
-	getData('nba.csv', shootingGuard, powerForward, center, pointGuard)
-	
-	#shows some sample scatterplots using matplotlib and numpy. X-axis is age (weight). Y-axis is player efficiency (value)
-	makeGraph(shootingGuard, "Shooting Guard Statistics")
-	makeGraph(powerForward, "Power Forward Statistics")
-	makeGraph(center, "Center Statistics")
-	makeGraph(pointGuard, "Point Guard Statistics")
-
+	test = []
+	getData("nba-sorted.csv", test)
+	W = 200
+	n = len(test)
+	knapSack(W, getWeights(test), getValues(test), n)
 	return 0
 
-#Function takes a set of data and title as parameters and uses them to construct a scatterplot with a line of linear regression (line of best fit)
-def makeGraph(dataset, titleString):
-	value = []
-	age = []
-	for person in dataset:
-		value.append(float(person.value))
-		age.append(int(person.age))
-	fig, ax = plt.subplots(1,1)
-	ax.plot(np.unique(age), np.poly1d(np.polyfit(age, value, 1))(np.unique(age)), color='red')
-	plt.xlabel('Age')
-	plt.ylabel('Value (player efficency)')
-	plt.title(titleString)
-	ax.scatter(age,value)
-	ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
-	ax.yaxis.set_major_locator(ticker.MultipleLocator(2))
-	plt.show()
-	return
+def knapSack(maxWeight, weight, value, counter):
+	table = [[0 for runningWeight in range(maxWeight + 1)]
+		 for index in range(counter + 1)]
+	for index in range(counter + 1):
+		for runningWeight in range(maxWeight + 1):
+			if index == 0 or runningWeight == 0:
+				table[index][runningWeight] = 0
+			elif weight[index - 1] <= runningWeight:
+				table[index][runningWeight] = max(value[index - 1] + table[index - 1][runningWeight - weight[index - 1]], table[index - 1][runningWeight])
+			else:
+				table[index][runningWeight] = table[index - 1][runningWeight]
+	final = table[counter][maxWeight]
+	print("Max skill is " + str(final))
+	w = maxWeight
+	for index in range(counter, 0, -1):
+		if final <= 0:
+			return 0
+		if final == table[index - 1][runningWeight]:
+			continue
+		else:
+			print("weight - " + str(weight[index - 1]) + " value - " + str(value[index - 1]))
+			final = final - value[index - 1]
+			runningWeight = runningWeight - weight[index - 1]
 
-#Function reads data from a csv file and sorts players by position into one of 4 position arrays
-def getData(fileLocation, shootingGuard, powerForward, center, pointGuard):
+def getData(fileLocation, dataset):
 	with open(fileLocation, newline='') as data:
 		reader = csv.reader(data)
 		for row in reader:
-			if(row[2] == "SG"):
-				shootingGuard.append(player(row[0], row[1], row[4], row[3]))
-			elif(row[2] == "PF"):
-				powerForward.append(player(row[0], row[1], row[4], row[3]))
-			elif(row[2] == "PG"):
-				pointGuard.append(player(row[0], row[1], row[4], row[3]))	
-			elif(row[2] == "C"):
-				center.append(player(row[0], row[1], row[4], row[3]))	
+			dataset.append(player(row[0], row[1], float(row[4]), int(row[3])))
+
+def getWeights(players):
+	output = []
+	for i in players:
+		output.append(i.weight)
+	return output
+
+def getValues(players):
+	output = []
+	for i in players:
+		output.append(i.value)
+	return output
 
 main()
